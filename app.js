afd155ffa3d0f24308ab6f2135033bc4d567b43e
sandeep <itssandeepsv@gmail.com>
2024-05-29 19:42:16 -0400

    Reactapp

---
+++ app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const parent = React.createElement("h1",{},"I am sandy");
+root.render(parent);
