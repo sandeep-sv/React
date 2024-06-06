119a002967abb53bd901c56fc6fabdf02d70c85f
sandeep <itssandeepsv@gmail.com>
2024-06-06 17:21:50 -0400

    comit_ep3

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const parent = React.createElement("h1",{},"I am sandy");
-root.render(parent);
+//react element
+
+const Heading = ()=>(<h1 id="head" className="heading">namasthee</h1>);
+
+const Main = ()=>(
+  <div id="maindiv" className="div1">
+    <Heading/>
+    <h2>This is h2</h2>
+  </div>
+
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root")); 
+
+root.render(<Main/>);
